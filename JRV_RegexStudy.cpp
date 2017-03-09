1cdcbc6bda914fd916e00b588bf74cd04968f388
jrvaughan917 <j_r_vaughan@yahoo.com>
2017-03-08 19:33:56 -0600

    Add files via upload

--- JRV_RegexStudy.cpp
+++ JRV_RegexStudy.cpp
@@ -26,6 +26,14 @@ int main()
 	regex ddmm("[0-9]{1,2}(\/|-|.)[0-9]{1,2}"); // DD/MM & DD-MM & DD.MM
 	regex mmyy("[0-9]{1,2}(\/|-|.)[0-9]{2}"); // MM/YY & MM-YY & MM.YY
 	regex yyyy("[0-9]{4}"); // YYYY
+	regex phoneNumber("^\\(?([0-9]{3})\\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$"); // ### ### #### using (), -, ., and spaces
+	//regex phoneNumber("((\\(\\d{3}\\) ?)|(\\d{3}-))?\\d{3}-\\d{4}"); // ### ### ####
+	//regex phoneNumber1("[0-9]{3}-[0-9]{3}-[0-9]{4}"); // ###-###-####
+	//regex phoneNumber2("\\([0-9]{3}\\)[0-9]{3}-[0-9]{4}"); // (###)###-#### // need to escape the \ characters for string pattern
+	//regex emailAddress("(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+"); // example@examplemail.com
+	regex emailAddress("^([a-zA-Z0-9_.+-])+\\@(([a-zA-Z0-9-])+\\.)+([a-zA-Z0-9]{2,4})+$"); // example@examplemail.com
+
+	// convert regex to C++ string format: remove the / delimiters and escape the \ characters
 
 	string testString = "01/01/1990";
 	smatch match;
@@ -102,6 +110,26 @@ int main()
 		if (regex_match(input, yyyy))
 		{
 			cout << "Date(YYYY) "; noMatch = false;
+		}
+
+		if (regex_match(input, phoneNumber))
+		{
+			cout << "Phone Number(### ### ####) "; noMatch = false;
+		}
+
+		/*if (regex_match(input, phoneNumber1))
+		{
+			cout << "Phone Number(###-###-####) "; noMatch = false;
+		}
+
+		if (regex_match(input, phoneNumber2))
+		{
+			cout << "Phone Number((###)###-####) "; noMatch = false;
+		}*/
+
+		if (regex_match(input, emailAddress))
+		{
+			cout << "Email Address (example@examplemail.com) "; noMatch = false;
 		}
 
 		if (noMatch)
